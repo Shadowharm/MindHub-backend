f6b86688f2a444e4fad47eee1893703a1ed7a16d
max.prokhorov <pmv.maks@gmail.com>
2024-10-13 15:29:19 +0300

    feat(prisma): add schemas

--- src/logs/logs.middleware.ts
+++
@@ -1,9 +0,0 @@
-import logsService from './logs.service'
-import { Request, Response } from 'express'
-export default async function (req: Request, res: Response) {
-  try {
-    await logsService.saveLog(req.url, res.statusCode, res.locals.error ?? null)
-  } catch (e) {
-    console.error(e)
-  }
-}
--- src/users/users.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import usersController from './users.controller'
-
-
-const userRouter = Router()
-
-userRouter.get('/', usersController.getAll)
-
-export default userRouter
